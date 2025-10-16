2fc042cfd70cce6feefb2acdb90b6f74a1ccf875
Milli Srivastava <millisrivastava73@gmail.com>
2025-10-16 17:29:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,11 @@ const observer = new IntersectionObserver((entries, obs) => {
 }, { threshold: 0.3 });
 
 faders.forEach(el => observer.observe(el));
+// ---------- HAMBURGER MENU ----------
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
